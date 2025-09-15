6cb8c2f61d609adad4c62bc98b09fa7cb65e0c0f
chinasusukjaroen <chinasu.suk@dome.tu.ac.th>
2025-09-15 14:13:06 +0700

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,73 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+
+	private Long id;
+	@Column(name = "first_name", nullable = false)
+	private String firstname;
+	@Column(name = "last_name", nullable = false)
+	private String lastname;
+	@Column(name = "email", nullable = false, unique = true)
+	private String email;
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public User(Long id, String firstname, String lastname, String email) {
+		super();
+		this.id = id;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.email = email;
+	}
+
+	public User(String firstname, String lastname, String email) {
+		super();
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.email = email;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
